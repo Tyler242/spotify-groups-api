697dc2bd0818157923565fe42d8b4f64c66f7137
Tyler242 <77693574+Tyler242@users.noreply.github.com>
2023-11-29 09:16:19 -0700

    added secrets passcode

--- src/controllers/secrets.ts
+++ src/controllers/secrets.ts
@@ -1,5 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 
 export function getSecrets(req: Request, res: Response, next: NextFunction) {
-    res.status(200).json({ clientId: process.env.SPOTIFY_CLIENT_ID!, redirectUri: process.env.SPOTIFY_REDIRECT_URI! });
+    try {
+        const secretsToken = req.headers.authorization?.replace("Bearer ", "");
+        if (!secretsToken) {
+            throw new Error("Unauthorized")
+        }
+        const accessCode = process.env.secrets_access_code!;
+        if (secretsToken !== accessCode) {
+            throw new Error("Unauthorized")
+        }
+
+        return res.status(200).json({ clientId: process.env.SPOTIFY_CLIENT_ID!, redirectUri: process.env.SPOTIFY_REDIRECT_URI! });
+    } catch (err) {
+        return next(err);
+    }
 }
